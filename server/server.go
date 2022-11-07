b60e071a87652bf98b2867d5b289071cb7c91220
Eda Eren <eda@Edas-MacBook-Pro.local>
2022-11-07 14:31:36 +0000

    implemented getBook handler

--- server/server.go
+++ server/server.go
@@ -1,3 +1,6 @@
+// TODO: Improvements -> https://www.youtube.com/watch?v=nZaHOkiQCzw&t=497s&ab_channel=Fabrzy
+// TODO: Gorilla mux -> https://www.youtube.com/watch?v=5BIylxkudaE&ab_channel=GoWebExamples
+
 package main
 
 import (
@@ -21,19 +24,31 @@ func generateDB() []Book {
 }
 
 func getAllBooksHandler(w http.ResponseWriter, req *http.Request, db []Book) {
-	fmt.Fprintf(w, "\n")
-
-	// TODO: fix book price/float bug. Returns value with too many decimal places
 	for _, book := range db {
-		msg := fmt.Sprintf("name: %s, genre: %s, price: %f", book.name, book.genre, book.price)
-		fmt.Fprintf(w, msg, "\n")
-
+		bookPriceRounded := fmt.Sprintf("%.2f", book.price)
+		msg := fmt.Sprintf("name: %s | genre: %s | price: %s \n", book.name, book.genre, bookPriceRounded)
+		fmt.Fprintf(w, msg)
 	}
+}
 
+func getBookHandler(w http.ResponseWriter, req *http.Request, db []Book) {
+	param1 := req.URL.Query().Get("name")
+
+	for _, book := range db {
+		if book.name == param1 {
+			bookPriceRounded := fmt.Sprintf("%.2f", book.price)
+			msg := fmt.Sprintf("name: %s | genre: %s | price: %s \n", book.name, book.genre, bookPriceRounded)
+			fmt.Fprintf(w, msg)
+		} else {
+			msg := fmt.Sprint(w, "cannot find book of name ", param1)
+			fmt.Fprintf(w, "cannot find book of name ", msg)
+		}
+	}
 }
 
 func main() {
 
+	// TODO: add proper response
 	db := generateDB()
 
 	fmt.Println(db)
@@ -41,6 +56,10 @@ func main() {
 	// TODO: Learn closure
 	http.HandleFunc("/getAllBooks", func(w http.ResponseWriter, r *http.Request) {
 		getAllBooksHandler(w, r, db)
+	})
+
+	http.HandleFunc("/getBook", func(w http.ResponseWriter, r *http.Request) {
+		getBookHandler(w, r, db)
 	})
 
 	http.ListenAndServe(":8090", nil)
